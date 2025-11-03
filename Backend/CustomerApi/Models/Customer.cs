b02db392f795287afe18c54443c2d215b727b994
AhmadV <ahmedalmawash12@gmail.com>
2025-11-03 03:17:43 +0300

    frontend setup +

--- Backend/CustomerApi/Models/Customer.cs
+++ Backend/CustomerApi/Models/Customer.cs
@@ -1,7 +1,7 @@
 namespace CustomerApi.Models;
 public class Customer
 {
-    public int CustomerId { get; set; }      // primary key
+    public int CustomerId { get; set; }      
     public string CustomerName { get; set; } = "";
     public DateOnly DateOfBirth { get; set; }
     public char Gender { get; set; }
